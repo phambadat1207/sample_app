f53687e7f56f77676b75d9114bafa411b9421316
phambadat <phambadat.97@gmail.com>
2018-12-25 13:11:59 +0700

    Updated Gemfile file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
-
   def hello
     render html: "hello, world!"
+  end
+  def dat
+    render html: "Pham Ba Dat"
   end
 end
