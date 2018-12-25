7a4afbe418a3e5ee5300fbb7ae6f31cf0f1755c8
phambadat <phambadat.97@gmail.com>
2018-12-25 16:38:36 +0700

    Finish chapter 3-add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
-  end
-  def dat
-    render html: "Pham Ba Dat"
+    render html: "Hello Pham Ba Dat"
   end
 end
