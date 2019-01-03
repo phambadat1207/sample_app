4d54b71bcf4269e70c551f28808752cf8e441b18
phambadat <phambadat.97@gmail.com>
2019-01-03 09:07:48 +0700

    chapter 7: Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
+    @user = User.new(name: "Sample App User", email: "user@sample.app",
       password: "foobar", password_confirmation: "foobar")
   end
 
@@ -11,7 +11,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-    @user.name = ""
+    @user.name = "     "
     assert_not @user.valid?
   end
 
@@ -32,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should accept valid addresses" do
     valid_addresses = %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                          first.last@foo.jp alice+bob@baz.cn)
+                        first.last@foo.jp alice+bob@baz.cn)
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
