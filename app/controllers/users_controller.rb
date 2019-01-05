699b81efa5e0d04509f8e1abab61b5b579b2e28e
phambadat <phambadat.97@gmail.com>
2019-01-05 22:26:50 +0700

    chapter 10: Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t "layouts.messenger.no_data"
-    redirect_to root_path
+  before_action :load_user, only: %i(show edit update destroy correct_user)
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page]
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -14,17 +18,60 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t "static_pages.home.sample"
+      flash[:success] = t "controllers.concerns.welcome"
       redirect_to @user
     else
       render :new
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "controllers.concerns.update_profile"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.delete
+      flash[:success] = t "controllers.concerns.delete_user"
+      redirect_to users_path
+    else
+      flash[:danger] = t "controllers.concerns.unsuccessfully_user"
+      redirect_to root_path
+    end
+  end
+
   private
 
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "layouts.messenger.no_data"
+    redirect_to root_path
+  end
+
   def user_params
-    params.require(:user).permit :name, :email, :password,
-      :password_confirmation
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "controllers.concerns.please_lg"
+    redirect_to login_path
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
   end
 end
